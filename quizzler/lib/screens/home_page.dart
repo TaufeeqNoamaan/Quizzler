56ccfecca6fbab11fd89771d5afd1b6c72535204
TaufeeqNoamaan <89037322+TaufeeqNoamaan@users.noreply.github.com>
2022-11-22 09:55:26 +0530

    app working

--- quizzler/lib/screens/home_page.dart
+++ quizzler/lib/screens/home_page.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'Question 1',
+    'Question 2',
+    'Question 3',
+    'Question 4',
+    'Question 5',
+    'Question 6',
+    'Question 7',
+    'Question 8',
+    'Question 9',
+    'Question 10'
+  ];
+  List<bool> ans = [
+    false,
+    true,
+    true,
+    false,
+    true,
+    true,
+    false,
+    false,
+    false,
+    true
+  ];
+
+  int q = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -11,13 +43,13 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Expanded(
+            Expanded(
               flex: 5,
               child: Padding(
                 padding: EdgeInsets.all(10),
                 child: Center(
                   child: Text(
-                    'QUESTION GOES HERE!',
+                    questions[q],
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 25, color: Colors.black),
                   ),
@@ -28,7 +60,25 @@ class HomePage extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.all(15),
                 child: ElevatedButton(
-                  onPressed: null,
+                  onPressed: () {
+                    bool correctAns = true;
+                    setState(() {
+                      if (correctAns == ans[q]) {
+                        scoreKeeper.add(Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ));
+                        
+                      }
+                      else {
+                        scoreKeeper.add(const Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+                      }
+                      q = q + 1;
+                    });
+                  },
                   child: const Text('True'),
                   style: ButtonStyle(
                       backgroundColor:
@@ -40,7 +90,24 @@ class HomePage extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.all(15),
                 child: ElevatedButton(
-                  onPressed: null,
+                  onPressed: () {
+                    bool correctAns = false;
+                    setState(() {
+                      if (correctAns == ans[q]) {
+                        scoreKeeper.add(const Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ));
+                      }
+                      else {
+                        scoreKeeper.add(const Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+                      }
+                      q = q + 1;
+                    });
+                  },
                   child: const Text('False'),
                   style: ButtonStyle(
                       backgroundColor:
@@ -48,6 +115,9 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            Row(
+              children: scoreKeeper,
+            )
           ],
         ),
       ),
